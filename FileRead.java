592274750afa15434a6b7c983163ab010415e3c4
marrionmierr <111546913+marrionmierr@users.noreply.github.com>
2022-12-18 18:25:35 +0800

    Create FileRead.java

---
+++ FileRead.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.io.File;
+//import java.io.*;
+import java.io.FileNotFoundException;
+
+public class FileRead {
+        public static void main(String[] args) throws FileNotFoundException {
+            File inputFile = new File("C:\\Users\\pabilandokv\\Documents\\NetBeansProjects\\JavaFileHandling\\input.txt");
+            Scanner inFile = new Scanner(inputFile);
+            
+            try{
+                while(inFile.hasNextLine()){
+                    System.out.println(inFile.nextLine());
+                }
+                System.out.println("");
+            }
+            catch(Exception e) {
+                System.out.println("An error has occured" + e.getMessage());
+        
+            }
+        }
+     
+}
